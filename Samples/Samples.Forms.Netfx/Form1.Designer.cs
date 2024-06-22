14cda96ae12db5aa3dfe348f65c1f4dd1a88a4f3
Tocchann <tosiyuki@hh.iij4u.or.jp>
2024-06-22 18:03:39 +0900

    参考用に旧形式のフォルダ選択ダイアログも使えるように修正(Formsのみ)

--- Samples/Samples.Forms.Netfx/Form1.Designer.cs
+++ Samples/Samples.Forms.Netfx/Form1.Designer.cs
@@ -33,38 +33,43 @@
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Location = new System.Drawing.Point(24, 18);
+			this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(263, 12);
+			this.label1.Size = new System.Drawing.Size(515, 24);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Vista 形式のファイルダイアログでフォルダを選択します。";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(12, 33);
+			this.label2.Location = new System.Drawing.Point(24, 66);
+			this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(57, 12);
+			this.label2.Size = new System.Drawing.Size(111, 24);
 			this.label2.TabIndex = 1;
 			this.label2.Text = "フォルダ(&F):";
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(75, 30);
+			this.textBox1.Location = new System.Drawing.Point(150, 60);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(340, 19);
+			this.textBox1.Size = new System.Drawing.Size(676, 31);
 			this.textBox1.TabIndex = 2;
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(421, 28);
+			this.button1.Location = new System.Drawing.Point(842, 56);
+			this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.Size = new System.Drawing.Size(150, 46);
 			this.button1.TabIndex = 3;
 			this.button1.Text = "選択(&S)...";
 			this.button1.UseVisualStyleBackColor = true;
@@ -73,26 +78,39 @@
 			// button2
 			// 
 			this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.button2.Location = new System.Drawing.Point(219, 56);
+			this.button2.Location = new System.Drawing.Point(438, 112);
+			this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.Size = new System.Drawing.Size(150, 46);
 			this.button2.TabIndex = 4;
 			this.button2.Text = "終了(&E)";
 			this.button2.UseVisualStyleBackColor = true;
 			this.button2.Click += new System.EventHandler(this.button2_Click);
 			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(842, 112);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(150, 46);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "旧選択...";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
 			// Form1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
 			this.CancelButton = this.button2;
-			this.ClientSize = new System.Drawing.Size(508, 91);
+			this.ClientSize = new System.Drawing.Size(1016, 182);
+			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.MaximizeBox = false;
 			this.Name = "Form1";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -109,6 +127,7 @@
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
 	}
 }
 
--- Samples/Samples.Forms.Netfx/Form1.cs
+++ Samples/Samples.Forms.Netfx/Form1.cs
@@ -29,6 +29,7 @@ namespace Samples.Forms.Netfx
 			dlg.InitialFolder = baseFolder;
 			dlg.Title = "フォルダを選択してください";
 			dlg.AddPlace( baseFolder, Wankuma.SelectFolder.Forms.SelectFolder.FDAP.TOP );
+			// if( dlg.ShowDialog( this ) == DialogResult.OK )
 			if( dlg.ShowDialog() == DialogResult.OK )
 			{
 				textBox1.Text = dlg.SelectedPath;
@@ -38,6 +39,17 @@ namespace Samples.Forms.Netfx
 		private void button2_Click( object sender, EventArgs e )
 		{
 			Close();
+		}
+
+		private void button3_Click( object sender, EventArgs e )
+		{
+			var dlg = new FolderBrowserDialog();
+			dlg.Description = "フォルダを選択してください";
+			dlg.SelectedPath = textBox1.Text;
+			if( dlg.ShowDialog( this ) == DialogResult.OK )
+			{
+				textBox1.Text = dlg.SelectedPath;
+			}
 		}
 	}
 }
